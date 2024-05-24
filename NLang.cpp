520033e294e5b0e393d72b468fcd56ee869292be
Nicolae Tugui <NicusorN5@proton.me>
2024-05-24 23:33:18 +0300

    Integer type usings

--- NLang.cpp
+++ NLang.cpp
@@ -1,6 +1,44 @@
 ﻿#include "pch.hpp"
 
+using ArgsList = const Vector<String>&;
+
+static i32 Main(ArgsList args);
+
 int main(int numArgs, char **args)
 {
-	
+	//put args** inside a std::vector for easier manipulation
+	std::vector<std::string> cmdArgs; 
+	for (auto&& n : iota(1, numArgs))
+	{
+		cmdArgs.push_back(std::string(args[n]));
+	}
+
+	return Main(cmdArgs);
 }
+
+static void ShowHelp()
+{
+	std::println("NLang : NLang compiler.");
+	std::println("Usage: NLang [files...] [-o OUTPUT] [-target:TARGET]");
+}
+
+static i32 Main(ArgsList args)
+{
+	Dictionary<String, int> options =
+	{
+		{"--help", 1},
+		{"-h", 1},
+		{"/?", 1},
+	};
+
+	for (auto&& argument : args)
+	{
+		switch (options[argument])
+		{
+		case 1:
+			ShowHelp();
+			return 0;
+		}
+	}
+}
+
--- pch.hpp
+++ pch.hpp
@@ -1,6 +1,31 @@
 #pragma once
 
+#include <string>
+using String = std::string;
+
 #include <print>
 #include <optional>
 #include <expected>
 #include <ranges>
+using std::views::iota;
+#include <vector>
+
+template<typename T>
+using Vector = std::vector<T>;
+#include <unordered_map>
+
+template<typename K, typename V>
+using Dictionary = std::unordered_map<K,V>;
+
+#include <cstdint>
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+#include <functional>
